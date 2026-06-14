e4d9e6c86ae70b3abc926b9f1cbd8118fdca3c4a
Meraz Rabbani <merazrabbani631@gmail.com>
2026-06-14 22:40:11 +0530

    Version 1.3 main.py
    ✅Search in history
    🔷First draft of customization
    ✅Color Adjustment
    ✅Size Adjustment

--- main.py
+++ main.py
@@ -1,97 +1,182 @@
-import qrcode as qr
-import os 
+import qrcode
+import os
 from datetime import datetime as dt
+
 def greet():
     print("=" * 40)
     print("🌟 WELCOME TO QR GENERATOR 🌟")
-    print("\n","=" * 40)
+    print("\n", "=" * 40)
+
 def history():
     while True:
         print("\n", "=" * 40)
-        print("\n1.View history\n2.Delete History\n3.Exit")
+        print("\n1.View history\n2.Search Qr\n3.Delete History\n4.Exit")
         print("\n", "=" * 40)
-        history_in= input("\nEnter your choice(in digit)")
+
+        history_in = input("\nEnter your choice(in digit)")
+
         if history_in == "1":
-            try:             
+            try:
                 with open("history.txt", "r") as file:
-                      readed = file.readlines()
-                      if len(readed) == 0:
-                          print("\n!!history is empty!!")
-                          continue
-                      else:
-                          for number, line in enumerate(readed, start=1):
-                              print(number, ".",line.strip()) 
-                          print("📊 Total QR Codes Generated: ", number)                  
-                          continue
+                    readed = file.readlines()
+
+                    if len(readed) == 0:
+                        print("\n!!history is empty!!")
+                        continue
+
+                    for number, line in enumerate(readed, start=1):
+                        print(number, ".", line.strip())
+
+                    print("📊 Total QR Codes Generated: ", len(readed))
+
             except Exception as e:
-                print("📁file not found", e)                                     
-            
+                print("📁 file not found", e)
+
         elif history_in == "2":
-            open("history.txt", "w").close()
-            continue
+            try:
+                search = input("\nEnter QR Name : ")
+
+                with open("history.txt", "r") as file:
+                    readeed = file.readlines()
+
+                for line in readeed:
+                    if search in line:
+                        print(line.strip())
+
+            except Exception as e:
+                print(e)
+
         elif history_in == "3":
-             return 
+            conform = input("\nAre You sure(yes/no) : ").lower()
+
+            if conform == "yes":
+                open("history.txt", "w").close()
+
+        elif history_in == "4":
+            return
+
         else:
-             print("Enter apprioprate value")
-             continue
+            print("Enter appropriate value")
+
 def menu():
     while True:
-         try:
+        try:
             print("=" * 40)
             print("\n1.Generate QR\n2.History\n3.Exit")
             print("=" * 40)
-            menu_choice = input("Enter you choice (in numbers) : ")
+
+            menu_choice = input("Enter your choice: ")
+
             if menu_choice == "1":
                 work()
+
             elif menu_choice == "2":
                 history()
+
             elif menu_choice == "3":
-                 break
-            else:
-                 print("Enter appriopriate input")
-         except Exception as e:
-             print("something went wrong", e)
-             continue
-def work():
-    url = input("\n 🔗 Enter Text / URL: ")
-    if url == "":
-        print("\nURLcannot be empty")
-        return work()
-    if not os.path.exists("QR codes"):
-        os.mkdir("QR codes")
-    file_n = input(" Enter the name : ")
-    if file_n == "":
-        print("\nName cannot be empty")
-        return work()
-    elif os.path.exists("QR codes/"+file_n+".png"):
-        print("⚠️ ", file_n, "already exists.")
-        return work()
-    make = qr.make(url)
-    make.save("QR codes/"+file_n+".png")
-    print("\n✅ QR Code Generated Successfully!")
-    print("📁 Saved as: ",file_n+".png")
-    t = dt.now()
-    current_time = t.strftime("%d/%m/%Y  %H:%M:%S   ")
-    with open("history.txt", "a") as file:
-        file.write(f"{url} | {file_n}.png | {current_time}\n")
-        start()
-def start():
-    while True:
-        try:
-            proced = input("\n🚀 Should we proceed? (yes/no): ").lower()
-
-            if proced == "yes":
-                work()
-
-            elif proced == "no":
-                print("\n🙏 Thank You For Using QR Generator!")
-                print("👋 Have a Great Day!")
                 break
 
             else:
-                print("⚠️ Please enter only 'yes' or 'no'\n")
-                continue
+                print("Enter appropriate input")
+
         except Exception as e:
-            print("❌ Something went wrong!",e)
-greet()
-menu()
+            print("something went wrong", e)
+
+def work():
+    url = input("\n🔗 Enter Text / URL: ")
+
+    if url == "":
+        print("URL cannot be empty")
+  return
+
+ qrcoe
+import os
+from datetime import datetime as dt
+
+def greet():
+    print("=" * 40)
+    print("🌟 WELCOME TO QR GENERATOR 🌟")
+    print("\n", "=" * 40)
+
+def history():
+    while True:
+        print("\n", "=" * 40)
+        print("\n1.View history\n2.Search Qr\n3.Delete History\n4.Exit")
+        print("\n", "=" * 40)
+
+        history_in = input("\nEnter your choice(in digit)")
+
+        if history_in == "1":
+            try:
+                with open("history.txt", "r") as file:
+                    readed = file.readlines()
+
+                    if len(readed) == 0:
+                        print("\n!!history is empty!!")
+                        continue
+
+                    for number, line in enumerate(readed, start=1):
+                        print(number, ".", line.strip())
+
+                    print("📊 Total QR Codes Generated: ", len(readed))
+
+            except Exception as e:
+                print("📁 file not found", e)
+
+        elif history_in == "2":
+            try:
+                search = input("\nEnter QR Name : ")
+
+                with open("history.txt", "r") as file:
+                    readeed = file.readlines()
+
+                for line in readeed:
+                    if search in line:
+                        print(line.strip())
+
+            except Exception as e:
+                print(e)
+
+        elif history_in == "3":
+            conform = input("\nAre You sure(yes/no) : ").lower()
+
+            if conform == "yes":
+                open("history.txt", "w").close()
+
+        elif history_in == "4":
+            return
+
+        else:
+            print("Enter appropriate value")
+
+def menu():
+    while True:
+        try:
+            print("=" * 40)
+            print("\n1.Generate QR\n2.History\n3.Exit")
+            print("=" * 40)
+
+            menu_choice = input("Enter your choice: ")
+
+            if menu_choice == "1":
+                work()
+
+            elif menu_choice == "2":
+                history()
+
+            elif menu_choice == "3":
+                break
+
+            else:
+                print("Enter appropriate input")
+
+        except Exception as e:
+            print("something went wrong", e)
+
+def work():
+    url = input("\n🔗 Enter Text / URL: ")
+
+    if url == "":
+        print("URL cannot be empty")
+        return
+
